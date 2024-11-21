f69af4eba917090f9e51e455eca5f9d06e0a5795
mariuskas1 <marius.kasparek@gmail.com>
2024-11-21 10:53:05 +0100

    added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LogInComponent } from './log-in/log-in.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: LogInComponent},
+    { path: 'main', component: MainPageComponent}
+];
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyCJIiLU3fdsW85OktOpe28MMXpdl_BhB4I",
+        authDomain: "dabubble-5d76a.firebaseapp.com",
+        projectId: "dabubble-5d76a",
+        storageBucket: "dabubble-5d76a.firebasestorage.app",
+        messagingSenderId: "1071462683293",
+        appId: "1:1071462683293:web:f9a7470fddf5b54152ba74"
+    }
+}
