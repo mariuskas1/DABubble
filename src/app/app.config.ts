1e08254fb70a050b075c2bbc6ec1c080012db0ce
mariuskas1 <marius.kasparek@gmail.com>
2024-11-21 10:38:25 +0100

    added firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"dabubble-5d76a","appId":"1:1071462683293:web:f9a7470fddf5b54152ba74","storageBucket":"dabubble-5d76a.firebasestorage.app","apiKey":"AIzaSyCJIiLU3fdsW85OktOpe28MMXpdl_BhB4I","authDomain":"dabubble-5d76a.firebaseapp.com","messagingSenderId":"1071462683293"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
