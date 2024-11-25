daf8f188f71c6391ccd9c6bc13ac193c9821f7f5
mariuskas1 <marius.kasparek@gmail.com>
2024-11-25 20:30:33 +0100

    workspace navigation

--- src/app/main-page/workspace/workspace-direct-messages/workspace-direct-messages.component.ts
+++ src/app/main-page/workspace/workspace-direct-messages/workspace-direct-messages.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-workspace-direct-messages',
   standalone: true,
   imports: [],
   templateUrl: './workspace-direct-messages.component.html',
-  styleUrl: './workspace-direct-messages.component.scss'
+  styleUrl: './workspace-direct-messages.component.scss',
+  animations: [
+    trigger('submenuAnimation', [
+      transition(':enter', [
+        style({ height: '0', opacity: 0 }),
+        animate('125ms ease-out', style({ height: '*', opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('125ms ease-in', style({ height: '0', opacity: 0 })),
+      ]),
+    ]),
+  ]
 })
 export class WorkspaceDirectMessagesComponent {
+  showSubmenu = true;
 
+
+  toggleDropdown(){
+    this.showSubmenu = !this.showSubmenu;
+  }
 }
