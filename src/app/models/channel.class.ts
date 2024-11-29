5e8bf76c28051923aad0a5a7c02886afddf84f46
mariuskas1 <marius.kasparek@gmail.com>
2024-11-29 14:40:35 +0100

    show members dialog

--- src/app/models/channel.class.ts
+++ src/app/models/channel.class.ts
@@ -8,12 +8,21 @@ export class Channel {
     messageIds: string[];
     threadIds: string[];
   
-    constructor(data: channelData) {
-      this.id = data.id ?? '';
-      this.userIds = data.userIds;
-      this.name = data.name;
-      this.description = data.description ?? '';
-      this.messageIds = data.messageIds ?? [];
-      this.threadIds = data.threadIds ?? [];
+    // constructor(data: channelData) {
+    //   this.id = data.id ?? '';
+    //   this.userIds = data.userIds;
+    //   this.name = data.name;
+    //   this.description = data.description ?? '';
+    //   this.messageIds = data.messageIds ?? [];
+    //   this.threadIds = data.threadIds ?? [];
+    // }
+
+    constructor(object?: any){
+      this.id =  object ? object.id : '';
+      this.userIds = object ? object.userIds : [];
+      this.name = object ? object.name : '';
+      this.description = object ? object.description : '';
+      this.messageIds = object ? object.messageIds : [];
+      this.threadIds = object ? object.threadIds : [];
     }
 }
