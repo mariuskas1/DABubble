a0e1f40039363b6f96eb1f476429341a379c561c
mariuskas1 <marius.kasparek@gmail.com>
2024-12-02 13:52:00 +0100

    edit channel form validation

--- src/app/main-page/add-channel-dialog/add-channel-dialog.component.ts
+++ src/app/main-page/add-channel-dialog/add-channel-dialog.component.ts
@@ -1,9 +1,12 @@
 import { Component, Output, EventEmitter, inject } from '@angular/core';
 import { Channel } from './../../models/channel.class';
+import { Observable } from 'rxjs';
+import { ChannelService } from '../../services/channel.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule, NgForm } from '@angular/forms';
-import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { Firestore, collection, addDoc, collectionData } from '@angular/fire/firestore';
 import { channelData } from '../../types/types';
+import { User } from '../../models/user.class';
 
 @Component({
   selector: 'app-add-channel-dialog',
@@ -19,32 +22,80 @@ export class AddChannelDialogComponent {
   mainDialogOpened: boolean = true;
   addPeopleDialogOpened: boolean = false;
   channel = new Channel();
+  channelMemberNames?: string[];
+  selectedOption: string = 'option1';
+  activeChannel: string | null = null;
+  activeChannelData!: Channel | null;
+  channels$!: Observable<Channel[]>;
+  allUsers$!: Observable<User[]>;
+  allUserChannels: Channel[] = [];
+  users: User[] = [];
+  
 
   firestore: Firestore = inject(Firestore);
 
-  channelMemberNames?: string[];
-  selectedOption: string = 'option1';
+  constructor(private channelService: ChannelService) {}
+
+
+
+  ngOnInit() {
+    this.channelService.activeChannel$.subscribe(channelId => {
+      this.activeChannel = channelId;
+      this.updateActiveChannelData();
+    });
+
+    this.getUserChannels();
+    this.getAllUsers();
+  }
+
+
+  getUserChannels(){
+    const userChannelsCollection = collection(this.firestore, 'channels' );
+    this.channels$ = collectionData(userChannelsCollection, { idField: 'id'}) as Observable<Channel[]>;
+    
+    this.channels$.subscribe((changes) => {
+      this.allUserChannels = Array.from(new Map(changes.map(channel => [channel.id, channel])).values());
+      this.updateActiveChannelData();
+    })
+  }
+
+
+  updateActiveChannelData(){
+    this.activeChannelData = this.allUserChannels.find(channel => channel.id === this.activeChannel) ?? null;
+  }
+
+
+  getAllUsers(){
+    const userCollection = collection(this.firestore, 'users');
+    this.allUsers$ = collectionData(userCollection, { idField: 'id'}) as Observable<User[]>;
+
+    this.allUsers$.subscribe((changes) => {
+      this.users = Array.from(new Map(changes.map(user => [user.id, user])).values());
+    })
+  }
+
   
     closeDialog(){
       this.dialogOpened = false;
       this.dialogStateChange.emit(this.dialogOpened);
     }
 
+
     stopPropagation(event: MouseEvent): void {
       event.stopPropagation();
     }
+
 
     getErrorMessage(errors:any): string{
       if(errors.required){
         return 'Bitte einen Namen eingeben.';
       }
-
       if (errors.minlength) {
         return `Der Name muss mindestens ${errors.minlength.requiredLength} Buchstaben lang sein.`;
       }
-
       return '';
     }
+
 
     displayAddPeopleDialog(ngForm: NgForm){
       if(ngForm.submitted && ngForm.form.valid){
@@ -54,9 +105,8 @@ export class AddChannelDialogComponent {
     }
 
     async createNewChannel(){
-      console.log(this.channel);
+      this.addMembersToChannel();
       try {
-        // add users to this.channel
         const channelCollection = collection(this.firestore, 'channels');
         await addDoc(channelCollection, { ...this.channel});
         this.channel = new Channel();
@@ -64,7 +114,16 @@ export class AddChannelDialogComponent {
       } catch (error) {
         console.error(error);
       }
+    }
 
+
+    addMembersToChannel(){
+      if (this.selectedOption === 'option1' && this.activeChannelData){
+        this.channel.userIds = this.activeChannelData.userIds;
+      } else if (this.selectedOption === 'option2'){
+        //push userIds to channelMembers on input first
+        //set this.channel.userIds to this.channelMembers
+      }
     }
 
 
